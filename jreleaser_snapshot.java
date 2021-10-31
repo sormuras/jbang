e21f08f1fd84bc12f963a6b8c40f51c975c82109
jreleaserbot <jreleaser@kordamp.org>
2021-10-31 20:49:09 +0000

    jreleaser v0.9.0-SNAPSHOT

---
+++ jreleaser_snapshot.java
@@ -0,0 +1,10 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 8+
+//REPOS jitpack
+//DEPS com.github.jreleaser.jreleaser:jreleaser:main-SNAPSHOT
+
+public class jreleaser_snapshot {
+    public static void main(String... args) throws Exception {
+        org.jreleaser.cli.Main.main(args);
+    }
+}
